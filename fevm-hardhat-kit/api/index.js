67948d878a5ef540a3c110208e4ce303428e2dec
Shannon Vaz <nonnahs.vaz@gmail.com>
2023-02-06 17:58:57 +0530

    Add cors

--- fevm-hardhat-kit/api/index.js
+++ fevm-hardhat-kit/api/index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const routes = require("./routes.js")
+const cors = require("cors")
 
 const app = express()
 const PORT = 5000
 const baseURL = "/api"
 
 app.use(express.json())
+app.use(cors())
 
 app.use(baseURL, routes)
 
